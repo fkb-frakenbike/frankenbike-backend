dae9a4486faa1064d48cabd7caf7dfd9fe244d1d
Carlos Oliveira <carlos.oliveira@believe.com>
2025-03-21 12:01:03 +0100

    chore(orm): deleted name propriety

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -17,9 +17,6 @@ class User
     #[ORM\Column(type: 'integer')]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $name = null;
-
     #[ORM\Column(length: 255, unique: true)]
     private ?string $email = null;
 
@@ -57,18 +54,6 @@ class User
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): static
-    {
-        $this->name = $name;
-
-        return $this;
     }
 
     public function getEmail(): ?string
