1e25515a9a751f67f027512aac12d0a7a7825163
CarlosPTN <carlosmioli1@gmail.com>
2025-05-29 17:18:48 +0200

    fix(token): added class CookieTokenExtractor

---
+++ src/Security/CookieTokenExtractor.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Security;
+
+use Lexik\Bundle\JWTAuthenticationBundle\TokenExtractor\TokenExtractorInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class CookieTokenExtractor implements TokenExtractorInterface
+{
+    private string $cookieName;
+
+    public function __construct(string $cookieName= 'AUTH_TOKEN_COOKIE')
+    {
+        $this->cookieName = $cookieName;
+    }
+    public function extract(Request $request): ?string
+    {
+        return $request->cookies->get($this->cookieName);
+    }
+}
