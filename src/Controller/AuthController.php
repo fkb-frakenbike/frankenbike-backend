9fd73b19aaed8081b499e10584a0ed0825675ac6
Carlos <carlosmioli1@gmail.com>
2025-06-03 10:39:32 +0200

    fix(costumAuthenticator):deleted

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -9,10 +9,8 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Cookie;
-use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 
 class AuthController extends AbstractController
@@ -30,24 +28,24 @@ class AuthController extends AbstractController
         $email = $data['email'] ?? '';
         $password = $data['password'] ?? '';
 
-        // Étape 1: Récupérer l'utilisateur
+        // Step 1: recover the user
         $user = $em->getRepository(User::class)->findOneBy(['email' => $email]);
         if (!$user) {
             return new JsonResponse(['error' => 'User not found'], Response::HTTP_UNAUTHORIZED);
         }
-        // Étape 2: Vérifier le mot de passe
+        // Step 2: Verify the password
         if (!$passwordHasher->isPasswordValid($user, $password)) {
             return new JsonResponse(['error' => 'Invalid credentials'], Response::HTTP_UNAUTHORIZED);
         }
-        // Étape 3: Générer le JWT
+        // Step 3:Generate the JWT
         $token = $jwtManager->create($user);
 
-        // Étape 4: Mettre le JWT dans un cookie sécurisé
+        // Step 4: Add the JWT in the secured cookie
         $response = new JsonResponse(['message' => 'Connected']);
         $response->headers->setCookie(
             Cookie::create('AUTH_TOKEN_COOKIE', $token)
                 ->withHttpOnly(true)
-                ->withSecure(true) // Important en prod !
+                ->withSecure(true) // Important in prod
                 ->withSameSite('Lax')
                 ->withPath('/')
                 ->withExpires(time() + 60 * 60 * 24 * 30)
