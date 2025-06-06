32c7c0fc326f80c96ca377ba9c4f0f89b5f9d730
Carlos <carlosmioli1@gmail.com>
2025-06-06 12:11:53 +0200

    feat(FKB-51): Remember me implementation

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -27,6 +27,7 @@ class AuthController extends AbstractController
         $data = json_decode($request->getContent(), true);
         $email = $data['email'] ?? '';
         $password = $data['password'] ?? '';
+        $rememberMe = $data['rememberMe'] ?? false;
 
         // Step 1: recover the user
         $user = $em->getRepository(User::class)->findOneBy(['email' => $email]);
@@ -37,23 +38,56 @@ class AuthController extends AbstractController
         if (!$passwordHasher->isPasswordValid($user, $password)) {
             return new JsonResponse(['error' => 'Invalid credentials'], Response::HTTP_UNAUTHORIZED);
         }
-        // Step 3:Generate the JWT
-        $token = $jwtManager->create($user);
 
-        // Step 4: Add the JWT in the secured cookie
-        $response = new JsonResponse(['message' => 'Connected']);
-        $response->headers->setCookie(
-            Cookie::create('AUTH_TOKEN_COOKIE', $token)
+        if($rememberMe) {
+            $now = new \DateTimeImmutable();
+            $later = $now->modify('+30 days');
+            $payload = [
+                'exp' => $later->getTimestamp(),
+            ];
+            $jwt = $jwtManager->create($user, $payload);
+        } else {
+            $jwt = $jwtManager->create($user);
+        }
+
+        $response = new JsonResponse(['message' => 'Connected'], Response::HTTP_OK);
+
+
+        if($rememberMe) {
+            $expiresAt = time()+(60*60*24*30);
+            $cookie = Cookie::create('AUTH_TOKEN_COOKIE', $jwt)
                 ->withHttpOnly(true)
-                ->withSecure(true) // Important in prod
-                ->withSameSite('Lax')
+                ->withSecure(true)
+                ->withSameSite("Lax")
                 ->withPath('/')
-                ->withExpires(time() + 60 * 60 * 24 * 30)
-        );
+                ->withExpires($expiresAt);
+        } else {
+            $cookie = Cookie::create('AUTH_TOKEN_COOKIE', $jwt)
+                ->withHttpOnly(true)
+                ->withSecure(true)
+                ->withSameSite("Lax")
+                ->withPath('/');
+        }
+
+
+        $response->headers->setCookie($cookie);
         return $response;
     }
 
     #[Route('/api/logout', name: 'api_logout', methods: ['POST'])]
-    public function logout(){
+    public function logout(): JsonResponse
+    {
+        $expired = Cookie::create('AUTH_TOKEN_COOKIE', '')
+            ->withHttpOnly(true)
+            ->withSecure(true)  // keep settings consistent
+            ->withSameSite('Lax')
+            ->withPath('/')
+            ->withExpires(1)    // 1 second after 1970 => removed by browser
+        ;
+
+        $response = new JsonResponse(['message' => 'Logged out']);
+        $response->headers->setCookie($expired);
+
+        return $response;
     }
 }
