60865cc45932b670309547567767adebc92d147b
Anael2902 <anael.toula@gmail.com>
2025-08-15 19:22:37 +0200

    feat(FKB-62: modify the SubUserController with the api/me because In my front I want a redirection and an 401 error if I'm not login and I'm going on the page timeline

--- src/Controller/SubUserController.php
+++ src/Controller/SubUserController.php
@@ -3,47 +3,39 @@
 declare(strict_types=1);
 
 namespace App\Controller;
+
 use App\Entity\Project;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class SubUserController extends UserController
 {
-    public function __construct( private EntityManagerInterface $em, SerializerInterface $serializer)
-    {}
+    private EntityManagerInterface $em;
+    private SerializerInterface $serializer;
+
+    public function __construct(EntityManagerInterface $em, SerializerInterface $serializer)
+    {
+        $this->em = $em;
+        $this->serializer = $serializer;
+    }
 
     #[Route('/api/me', name: 'me', methods: ['GET'])]
     public function apiMe(): JsonResponse
     {
         $user = $this->getUser();
-        $projects = $this->em->getRepository(Project::class)->findBy(['user' => $user]);
 
-        $projectsJson = $serializer->serialize($projects, 'json');
-//        $projectData = [];
-//        foreach ($projects as $project) {
-//            $projectData[] = [
-//                'id' => $project->getId(),
-//                'title' => $project->getTitle(),
-//                'description' => $project->getDescription(),
-//                'imageUrl' => $project->getImageUrl(),
-//                'createdAt' => $project->getCreatedAt()?->format('c'),
-//                'updatedAt' => $project->getUpdatedAt()?->format('c'),
-//            ];
-//        }
+        if (!$user) {
+            return new JsonResponse(['error' => 'Unauthorized'], JsonResponse::HTTP_UNAUTHORIZED);
+        }
 
-        # return $this->json($this->getUser());
-//        return new JsonResponse([
-//            'id' => $user->getId(),
-//            'email' => $user->getEmail(),
-//            'roles' => $user->getRoles(),
-//            'createdAt' => $user->getCreatedAt()?->format('c'),
-//            'projects' => $projectData, // Optional, or you can fill with IDs or names if needed
-//            'likes' => [],    // Same
-//            'userIdentifier' => method_exists($user, 'getUserIdentifier') ? $user->getUserIdentifier() : null,
-//        ]);
-        return new JsonResponse($projectsJson,JsonResponse::HTTP_OK, [], true);
+        // Simple réponse pour test
+        return new JsonResponse([
+            'id' => $user->getId(),
+            'email' => $user->getEmail(),
+            'username' => $user->getUserIdentifier(),
+        ]);
     }
-
 }
+
